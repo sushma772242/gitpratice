89504c68c3be4682cdc28d22109ebf3efe82f588
sushma772242 <sushmasonnad17@gmail.com>
2026-07-08 16:59:49 +0530

    Initial React Node project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.text())
+      .then((data) => setMessage(data));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Simple React + Node.js Project</h1>
+
+      <h2>Message from Backend</h2>
+
+      <h3>{message}</h3>
+    </div>
+  );
+}
+
+export default App;
